7651d68b03a313bb0aee49a0b4307e90975d2edc
bagas prasetya <bagasprasetya1702@gmail.com>
2016-10-02 22:21:57 +0700

    initial commit

---
+++ src/js/script.js
@@ -0,0 +1,15 @@
+function validate() {
+
+	var password = document.getElementById("password").value;
+
+	if(password == "w") {
+		
+		setTimeout(function() {
+			document.getElementById("successtext").innerHTML = "Halooooo Wildaa Selamat Datang!!!!!!";
+			document.getElementById("next").style.display = "block"
+		}, 1000)
+	}
+	else {
+		alert("Siapa lo");
+	}
+}
